03006556b9eb29d4dfede22de8c05c69ac1cf4d4
Elias Panula <elias.panula@gmail.com>
2018-01-17 21:54:18 +0200

    refactored

---
+++ helper.js
@@ -0,0 +1,80 @@
+function makeQuery(data) {
+    var now = new Date();
+    var time = now.toString().split(' ')[4];
+
+    var month = (now.getMonth() + 1).toString();
+    if (month.length == 1) month = '0' + month;
+
+    var d = now.getDate().toString();
+    if (d.length == 1) d = '0' + d;
+
+    var year = now.getFullYear();
+    var date = "" + year + "-" + month + "-" + d;
+
+    var from = data.start_coord;
+    var to = data.end_coord;
+
+    var fromPlace = data.start;
+    var toPlace = data.end;
+
+    var query = `{
+        plan(
+        fromPlace: "` + fromPlace + `",
+        from: {lat: ` + from.lat + `, lon: ` + from.lon + `},
+        toPlace: "` + toPlace + `",
+        to: {lat: ` + to.lat + `, lon: ` + to.lon + `},
+        date: "` + date + `",
+        time: "` + time + `"
+        numItineraries: 1,
+        modes: "BUS,WALK",
+        walkReluctance: 2.1,
+        walkBoardCost: 600,
+        minTransferTime: 600,
+        walkSpeed: 1.7,
+        ) {
+        itineraries{
+            walkDistance,
+            duration,
+            legs {
+                mode
+                route {
+                    shortName
+                }
+                startTime
+                endTime
+                from {
+                    lat
+                    lon
+                    name
+                    stop {
+                        code
+                        name
+                    }
+                },
+                to {
+                    lat
+                    lon
+                    name
+                },
+                agency {
+                    id
+                },
+                distance
+                legGeometry {
+                    length
+                    points
+                }
+            }
+        }
+        }
+    }`
+
+    return query;
+}
+
+var address = 'http://api.digitransit.fi/routing/v1/routers/hsl/index/graphql'
+
+module.exports = {
+    makeQuery,
+    address
+}
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var curl = require('curl');
 var app = express();
 var bParser = require('body-parser');
 var axios = require('axios');
-
+var helper = require('./helper.js')
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
@@ -21,97 +21,13 @@ app.use(bParser.urlencoded({
 
 app.listen(3001);
 
-var address = 'http://api.digitransit.fi/routing/v1/routers/hsl/index/graphql'
-
-var counter = 0
-
-function makeQuery(data) {
-    console.log(data)
-    console.log(data.end)
-    console.log(data.start)
-    console.log(data.end_coord)
-    console.log(data.start_coord)
-    var now = new Date();
-    var time = now.toString().split(' ')[4];
-
-    var month = (now.getMonth() + 1).toString();
-    if (month.length == 1) month = '0' + month;
-
-    var d = now.getDate().toString();
-    if (d.length == 1) d = '0' + d;
-
-    var year = now.getFullYear();
-    var date = "" + year + "-" + month + "-" + d;
-
-    var from = data.start_coord;
-    var to = data.end_coord;
-
-    var fromPlace = data.start;
-    var toPlace = data.end;
-
-    var query = `{
-        plan(
-        fromPlace: "` + fromPlace + `",
-        from: {lat: ` + from.lat + `, lon: ` + from.lon + `},
-        toPlace: "` + toPlace + `",
-        to: {lat: ` + to.lat + `, lon: ` + to.lon + `},
-        date: "` + date + `",
-        time: "` + time + `"
-        numItineraries: 1,
-        modes: "BUS,WALK",
-        walkReluctance: 2.1,
-        walkBoardCost: 600,
-        minTransferTime: 600,
-        walkSpeed: 1.7,
-        ) {
-        itineraries{
-            walkDistance,
-            duration,
-            legs {
-                mode
-                route {
-                    shortName
-                }
-                startTime
-                endTime
-                from {
-                    lat
-                    lon
-                    name
-                    stop {
-                        code
-                        name
-                    }
-                },
-                to {
-                    lat
-                    lon
-                    name
-                },
-                agency {
-                    id
-                },
-                distance
-                legGeometry {
-                    length
-                    points
-                }
-            }
-        }
-        }
-    }`
-
-    return query;
-}
-
 function queryHSL(data, callback) {
-    var query = makeQuery(data)
-    curl.post(address, query, { "Content-Type": "application/graphql" }, function (err, response, body) {
+    var query = helper.makeQuery(data)
+    curl.post(helper.address, query, { "Content-Type": "application/graphql" }, function (err, response, body) {
         if (err) {
             callback({ message: 'Internal error.', status: 500 })
         }
         else {
-            console.log("from hsl query: " + response.statusCode);
             if (response.statusCode != 200) callback({ message: 'Could not get data.', status: 450 })
             else {
                 var json = JSON.parse(body).data.plan;
@@ -171,9 +87,6 @@ function queryHSL(data, callback) {
 }
 
 function makeCoordsQuery(location) {
-
-    console.log('query from: ' + location);
-
     var query = `
         {
             stops(name: "`+ location + `"){
@@ -188,7 +101,7 @@ function makeCoordsQuery(location) {
 function setCoords(coords, query, callback) {
     axios({
         method: 'post',
-        url: address,
+        url: helper.address,
         data: query,
         headers: { "Content-Type": "application/graphql" }
     }).then(function (response) {
@@ -199,7 +112,7 @@ function setCoords(coords, query, callback) {
             coords.lon = stop.lon;
             callback({ lat: stop.lat, lat: stop.lon, status: 200 })
         } else {
-            callback({ message: 'No stops found.', status: 400 })
+            callback({ message: 'Sorry no stops found! Please try again', status: 400 })
         }
     }).catch(function (error) {
         if (error.response.status == 500) {
@@ -216,36 +129,27 @@ function printArray(data) {
     }
 }
 
-//queryN(1);
 function queryN(n) {
-    //for (var i = 0; i < n; i++) {
-    axios.post(address, query, { "Content-Type": "application/graphql" }).then(function (body, b) { console.log('curl then success? ' + JSON.parse(body).data) }).catch(function (error) { console.log(error.response.statusText) });
-    //}
+    axios.post(helper.address, query, { "Content-Type": "application/graphql" }).then(function (body, b) { console.log('curl then success? ' + JSON.parse(body).data) }).catch(function (error) { console.log(error.response.statusText) });
 }
 
 app.post('/data', function (req, res) {
-    res.set('Content-Type', 'application/json')
-    res.set('Access-Control-Allow-Origin', '*')
     queryHSL(req.body, function (response) {
         res.send(response);
     })
 })
 
 app.post('/set', function (req, res) {
-    var data = req.body
-    var start = data.start;
-    var end = data.end;
-    var counter = 0
+    var start = req.body.start;
+    var end = req.body.end;
     setAllCoords(start, end, function (response) {
         res.send(response)
     })
 });
 
 function setAllCoords(start, end, callback) {
-
     var start_coord = { lat: 0, lon: 0 };
     var end_coord = { lat: 0, lon: 0 };
-
     var start_query = makeCoordsQuery(start);
     var end_query = makeCoordsQuery(end);
     setCoords(start_coord, start_query, function (response) {
@@ -253,7 +157,7 @@ function setAllCoords(start, end, callback) {
             setCoords(end_coord, end_query, function (response) {
                 if (response.status == 200) {
                     callback({
-                        message: 'Coordinates are set',
+                        message: `Please wait.`,
                         data:
                             {
                                 start: start,
